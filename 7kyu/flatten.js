2bbf7260f8244496952e350d2fed04eef680ae07
Adrien Dorce <dorceadrien@gmail.com>
2022-11-27 14:37:17 -0500

    added new file

---
+++ 7kyu/flatten.js
@@ -0,0 +1,12 @@
+//Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+//PREP
+//write function that flattens array
+//only do one level 
+
+
+function flatten(arr){
+    return arr.flat()
+}
+  
+  //flatten([2,4,5], [5,6,7]) // => [2,4,5,5,6,7]
